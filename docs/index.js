7b17a98d2716d9db4efdd88373502d8a3997608a
SoCuul <socuul@outlook.com>
2022-07-02 01:58:14 -0700

    Shuffle answers array

--- docs/index.js
+++ docs/index.js
@@ -9,6 +9,23 @@ function showToast (message, type, duration) {
     })
 }
 
+//Random functions
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [ array[currentIndex], array[randomIndex] ] = [ array[randomIndex], array[currentIndex] ];
+    }
+  
+    return array;
+}
+
 const mainVue = Vue.createApp({
     data() {
         return {
@@ -56,6 +73,7 @@ const mainVue = Vue.createApp({
             //Update variables
             this.previousQuestion = index
             this.currentQuestion = question
+            this.currentQuestion.answers = shuffle(question?.answers)
         },
         async submitAnswer (value) {
             //Check if pending submit
